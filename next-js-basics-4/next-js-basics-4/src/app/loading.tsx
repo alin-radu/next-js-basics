9032bca10275091e55166b249616703c5b6bd49e
Alin RADU <alin.radu.cristian@gmail.com>
2023-12-27 08:48:13 +0200

    remove next-js-basics-4

--- next-js-basics-4/next-js-basics-4/src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function SnippetLoadingPage() {
-  return (
-    <div>
-      <h1 className="text-xl bold">Main, Loading...</h1>
-    </div>
-  );
-}
--- next-js-basics-4/next-js-basics-4/src/app/snippets/[id]/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function SnippetLoadingPage() {
-  return (
-    <div>
-      <h1 className="text-xl bold">Snippet, Loading...</h1>
-    </div>
-  );
-}
